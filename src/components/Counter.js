87eea7d2d306012e0e062532d263a7654dea19a4
sskiragu <skiragu18@gmail.com>
2022-08-29 18:30:42 +0300

    Counter app using class component example 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,12 +8,17 @@ export default class Counter extends Component {
             count: 0
         }
     }
+
+    increaseCounter = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
   render() {
     return (
       <div>
         <h1>Counter App using Class component</h1>
         <p>{this.state.count}</p>
-        <button onClick={() => this.setState({count: this.state.count + 1})}>Increase Count</button>
+        <button onClick={this.increaseCounter}>Increase Count</button>
         <button onClick={() => this.setState({count: this.state.count - 1})}>Decrease Count</button>
       </div>
     )
