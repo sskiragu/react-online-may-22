d406ba0af08db328d565412dfef5eb402fa5391e
sskiragu <skiragu18@gmail.com>
2022-08-29 18:39:05 +0300

    Counter app using functional component example

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class Counter extends Component {
+function Counter() {
 
-    constructor(){
-        super()
-        this.state = {
-            count: 0
-        }
+    const [count, setCount] = useState(0)
+
+    const increaseCount = () => {
+        setCount(count + 1)
     }
-
-    increaseCounter = () => {
-        this.setState({count: this.state.count + 1})
-    }
-
-  render() {
-    return (
-      <div>
-        <h1>Counter App using Class component</h1>
-        <p>{this.state.count}</p>
-        <button onClick={this.increaseCounter}>Increase Count</button>
-        <button onClick={() => this.setState({count: this.state.count - 1})}>Decrease Count</button>
-      </div>
-    )
-  }
+  return (
+    <div>
+        <h1>Counter App using Functional Componet - useState hook</h1>
+        <p>{count}</p>
+        <button onClick={increaseCount}>Increase Counter</button>
+    </div>
+  )
 }
+
+export default Counter
