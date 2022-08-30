1626de0122db3783678278d46b100333c2de5d62
sskiragu <skiragu18@gmail.com>
2022-08-30 18:59:47 +0300

    React form using functional component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import './App.css'
 import Login from './components/Login';
 import NavBar from './components/NavBar';
-// import Register from './components/Register';
-import SignUp from './components/SignUp';
+import Register from './components/Register';
+// import SignUp from './components/SignUp';
 import Counter from './components/Counter'
 
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
             <NavBar />
             <Routes>
               <Route path='/login' element={<Login />}/>
-              <Route path='/register' element={<SignUp />}/>
+              <Route path='/register' element={<Register />}/>
               <Route path='/counter' element={<Counter username='admin'/>}/>
             </Routes>
           </BrowserRouter>
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,33 +3,41 @@ import { Link } from "react-router-dom";
 
 function Register(){
 
-    const [firstname, setFirstname] = useState("John")
-    const [lastname, setLastname] = useState("Doe")
+    const [username, setUsername] = useState('sam')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(username);
+    }
+
     return(
         <div>
             <h1>Register here - using Functional component.</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
 
                 <div>
-                    <input type='text'  placeholder='Enter username'/>
+                    <input type='text' value={username} onChange={handleUsernameChange} placeholder='Enter username'/>
                 </div>
 
                 <div>
-                    <input type='email'  placeholder='Enter email'/>
+                    <input type='email' value={email} placeholder='Enter email'/>
                 </div>
 
                 <div>
-                    <input type='password'  placeholder='Enter password'/>
+                    <input type='password' value={password} placeholder='Enter password'/>
                 </div>
 
                 <div>
-                    <button type='button'>Register</button>
+                    <button type='submit'>Register</button>
                 </div>
             </form>
             <p><Link to='/'>Login</Link></p>
-
-            <p>{firstname}</p>
-            <p>{lastname}</p>
         </div>
     )
 }
